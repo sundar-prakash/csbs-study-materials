7450fca36dc108883ad4086b6d08e4d3165d6cf5
okayitssundar <psundarprakash603@gmail.com>
2024-05-26 12:11:35 +0530

    first commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,40 +1,40 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'CSBS Study Materials',
-  tagline: 'S.A. Enginnering College',
-  favicon: 'img/favicon.ico',
+  title: "CSBS Study Materials",
+  tagline: "S.A. Enginnering College",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://csbs.netlify.app',
+  url: "https://csbs.netlify.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'okayitssundar', // Usually your GitHub org/user name.
-  projectName: 'csbs-study-materials', // Usually your repo name.
+  organizationName: "okayitssundar", // Usually your GitHub org/user name.
+  projectName: "csbs-study-materials", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
@@ -48,7 +48,7 @@ const config: Config = {
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -56,67 +56,67 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'CSBS Study Materials',
+      title: "CSBS Study Materials",
       logo: {
-        alt: 'CSBS Study Materials',
-        src: 'img/logo.svg',
+        alt: "CSBS Study Materials",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Notes',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Notes",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/okayitssundar/csbs-study-materials',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/okayitssundar/csbs-study-materials",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Notes',
-              to: '/docs/intro',
+              label: "Notes",
+              to: "/docs/intro",
             },
           ],
         },
         {
-          title: 'Socials',
+          title: "Socials",
           items: [
             {
-              label: 'Website',
-              href: 'https://sundar.zingbizz.com/',
+              label: "Website",
+              href: "https://sundar.zingbizz.com/",
             },
             {
-              label: 'Instagram',
-              href: 'https://www.instagram.com/okayitssundar',
+              label: "Instagram",
+              href: "https://www.instagram.com/okayitssundar",
             },
             {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/in/okayitssundar',
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/in/okayitssundar",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              to: "/blog",
             },
             {
-              label: 'Framework-GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "Framework-GitHub",
+              href: "https://github.com/facebook/docusaurus",
             },
           ],
         },
